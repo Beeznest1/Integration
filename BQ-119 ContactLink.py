8063164a658d804a9ff93937429b9170a4ed38e0
Comrade604 <comrade604@gmail.com>
2023-02-17 15:30:21 -0800

    Final correct version

--- BQ-119 ContactLink.py
+++ BQ-119 ContactLink.py
@@ -1,15 +1,31 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from time import sleep
 
-s = Service(executable_path='chromedriver.exe')
-
-driver = webdriver.Chrome(service=s)
+# Create an instance of the web driver
+driver = webdriver.Chrome()
 driver.maximize_window()
-driver.implicitly_wait(5)
 
-driver.get('https://test.beeznests.com/')
-driver.find_element(By.XPATH, "/html/body/div/div[9]/div[1]/div[3]/a[4]").click()
-driver.implicitly_wait(5)
-sleep(5)
+# Navigate to the webpage that contains the mailto link
+driver.get("https://test.beeznests.com/")
+
+driver.find_element(By.XPATH, "//body").send_keys(Keys.END)
+
+# Locate the mailto link on the webpage
+mailto_link = driver.find_element(By.LINK_TEXT, "Contact Us")
+
+assert mailto_link.is_displayed()
+
+href_value = mailto_link.get_attribute("href")
+
+expected_url = "mailto:jin.liu@beeznests.com"
+
+assert href_value == expected_url
+
+if href_value == expected_url:
+    print("The email link is correct:")
+    print(href_value)
+else:
+    print("Email validation failed.")
+
+driver.quit()
